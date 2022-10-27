57ce9710cb8e7b4d13af5025142cea2bc483299d
Peter Neville <neville.peter.2000@gmail.com>
2022-10-27 17:51:26 -0500

    algunos cambios

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ dotenv.config();
 const db = new Sequelize (
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD, {
+  process.env.DB_PASSWORD || "johnpeter83", {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
